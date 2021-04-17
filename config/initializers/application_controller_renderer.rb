400dd45b1ae51745f418199aee970439c9a0866a
ponxpon <ponponpon816@gmail.com>
2021-04-17 13:28:40 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '28b7e027fa2bddd3b1d9b175e12abce41122485d11d43e026debaf62830d08903805bc093eaf7add316a9e0d8d62ecec1fff977ab356dc2a607e54dca8aa82f5'
